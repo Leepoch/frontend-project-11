1e56a1b0215f9fc2ea7f68a7b8d8f9040a9b189c
Daniil <daniiltinkovv@gmail.com>
2022-12-27 16:37:04 +0300

    add yup

---
+++ src/init.js
@@ -0,0 +1,50 @@
+
+import './scss/styles.scss'
+import Example from './Example.js';
+import { object, string } from 'yup';
+import onChange from 'on-change';
+import cb from './view.js';
+
+
+export default () => {
+  const element = document.getElementById('point');
+  const obj = new Example(element);
+  obj.init();
+};
+
+const app = () => {
+  const state = {
+    repeatUrls: [],
+    inputValue: '',
+    inputState: 'filling',
+  }
+
+  const watchedState = onChange(state, () => cb(state));
+  const form = document.querySelector('.rss-form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let schema = object({
+      website: string().url(),
+    });
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    state.inputValue = url;
+    schema.isValid({ website: state.inputValue}).then((bl) => {
+      if (bl === true && state.repeatUrls.includes(state.inputValue)) {
+        watchedState.inputState = 'exists';
+      }
+      if (bl === true && !state.repeatUrls.includes(state.inputValue)) {
+        watchedState.inputState = 'correct';
+        state.repeatUrls.push(watchedState.inputValue)
+      }
+      if (bl === false) {
+        watchedState.inputState = 'uncorrect';
+      }
+    });
+  })
+
+  return;
+}
+
+console.log(app());
